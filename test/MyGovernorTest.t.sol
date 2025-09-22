892fb48a4ff48abd6486f514c22e1c99e3751b91
mbarraldevs <mbarraldevs@outlook.com>
2025-09-22 12:06:39 +0200

    Finalized setUp test function but need work on adapting with wizard update

--- test/MyGovernorTest.t.sol
+++ test/MyGovernorTest.t.sol
@@ -15,9 +15,35 @@ contract MyGovernorTest is Test {
 
     address public USER = makeAddr("user");
     uint256 public constant USER_STARTING_BALANCE = 100 ether;
+    uint256 public constant MIN_DELAY = 3600; // 1 hour, no one can execute the passed proposal until 1 hour after
 
-    function setup() public {
-        govToken = new GovToken(msg.sender);
+    function setUp() public {
+        //We need the govToken and TimeLock to deploy the governor
+        govToken = new GovToken(USER);
         govToken.mint(USER, USER_STARTING_BALANCE);
+
+        vm.prank(USER);
+        govToken.delegate(USER);
+        timeLock = new TimeLock(MIN_DELAY, new address[](0), new address[](0));
+
+        //We can now deploy the governor
+        governor = new MyGovernor(govToken, timeLock);
+
+        bytes32 proposerRole = timeLock.PROPOSER_ROLE();
+        bytes32 executorRole = timeLock.EXECUTOR_ROLE();
+        //bytes32 adminRole = timeLock.TIMELOCK_ADMIN_ROLE();
+
+        timeLock.grantRole(proposerRole, address(governor));
+        timeLock.grantRole(executorRole, address(0)); //address(0) is everyone
+
+        //We need a contract to call through governance, so we deploy the Box contract
+        box = new Box();
+        //We need to transfer the ownership of the box contract to the timelock
+        box.transferOwnership(address(timeLock));
+    }
+
+    function testCantUpdateBoxWithoutGovernance() public {
+        vm.expectRevert();
+        box.store(1);
     }
 }
